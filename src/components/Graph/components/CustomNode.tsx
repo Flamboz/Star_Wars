a66cbc3f6363726b8543cdc587635afb3b002c5b
nadia <nadia0ostapyshyn@gmail.com>
2024-10-21 13:05:12 +0300

    add placeholder image if no image found

--- src/components/Graph/components/CustomNode.tsx
+++ src/components/Graph/components/CustomNode.tsx
@@ -1,19 +1,34 @@
 import { Handle, Position } from "@xyflow/react";
 import "./CustomNode.css";
 import { NodeData } from "../../../types";
+import { useState } from "react";
 
 interface CustomNodeProps {
   data: NodeData;
 }
 
-// CustomNode component: Represents a custom node in the React Flow graph, displaying character details along with an image. 
-// It includes input and output handles for connections to other nodes.
+// CustomNode component represents a node in the React Flow graph.
+// It displays character details along with an image, providing input and output handles for connecting to other nodes.
+// The component includes a loading spinner and a placeholder image to handle cases where the main image is still loading or fails to load.
 const CustomNode: React.FC<CustomNodeProps> = ({ data }) => {
+  const placeholderImage =
+    "https://placehold.co/100x200/gray/ffffff?text=No+Image";
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="node">
-      {data.imageUrl && (
-        <img src={data.imageUrl} alt={String(data.info?.[0].value)} />
-      )}
+      <div className={`image-container ${loading ? "loading" : ""}`}>
+        <img
+          src={data.imageUrl}
+          alt={String(data.info?.[0].value)}
+          onLoad={() => setLoading(false)}
+          onError={(e) => {
+            e.currentTarget.onerror = null;
+            e.currentTarget.src = placeholderImage;
+          }}
+        />
+        {loading && <div className="spinner">Loading...</div>}{" "}
+      </div>
       <div className="node__label">
         {data.info?.map((info, index) => (
           <div className="info-row" key={index}>
